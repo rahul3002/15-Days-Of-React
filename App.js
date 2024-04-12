c316285a37db4508922853df9ad3acd5ac17c4c3
rahul3002 <rahulbagal@Rahuls-MacBook-Air.local>
2024-04-12 20:24:17 +0530

    Refactor .res-card styles in index.css to adjust width and height

--- App.js
+++ App.js
@@ -4,9 +4,7 @@ import ReactDOM from "react-dom/client";
 const Logo = () => {
   const src =
     "https://img.freepik.com/free-vector/detailed-chef-logo-template_23-2148986823.jpg";
-  return (
-    <img className="logo" src={src}></img>
-  );
+  return <img className="logo" src={src}></img>;
 };
 
 const NavItems = () => {
@@ -31,7 +29,8 @@ const Header = () => {
   );
 };
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+  console.log(props);
   return (
     <div className="res-card">
       <img
@@ -39,7 +38,7 @@ const RestaurantCard = () => {
         alt="restaurant-logo"
         src="https://media.istockphoto.com/id/1345624336/photo/chicken-biriyani.jpg?s=612x612&w=0&k=20&c=adU_N0P-1SKMQLZu5yu7aPknfLLgbViI8XILqLP92A4= "
       />
-      <h3>Meghana Foods</h3>
+      <h3>{props.resName}</h3>
       <h4>Biriyani, North India, Asian</h4>
       <h4>4.4 stars</h4>
       <h4>38 minutes</h4>
@@ -55,6 +54,13 @@ const Body = () => {
         <input className="name" placeholder="search food"></input>
       </div>
       <div className="res-Container">
+        <RestaurantCard
+          resName="Meghana Foods"
+          Cusines="Biriyani, North India, Asian"
+        />
+        <RestaurantCard resName="KFC" Cusine="Burgers, FastFood" />
+        <RestaurantCard />
+        <RestaurantCard />
         <RestaurantCard />
         <RestaurantCard />
         <RestaurantCard />
